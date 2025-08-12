9da306aa3c132bb67aee265ef26cce775f77f7d7
codemine24 <codemine24@gmail.com>
2025-08-12 20:47:55 +0600

    basic edit added.

--- src/app/manage-companies/_components/company-table-v2.tsx
+++ src/app/manage-companies/_components/company-table-v2.tsx
@@ -1,10 +1,14 @@
+import { AddDocumentModal } from "@/components/document-form-modal";
 import AddIcon from "@mui/icons-material/Add";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import DoneIcon from "@mui/icons-material/Done";
 import EditIcon from "@mui/icons-material/Edit";
 import {
+  alpha,
   Box,
   Button,
+  Chip,
   FormControl,
   IconButton,
   Paper,
@@ -14,6 +18,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -22,7 +27,6 @@ import dayjs from "dayjs";
 import * as React from "react";
 import { CustomInputField } from "./custom-input";
 import { CustomSelect } from "./custom-select";
-import { AddDocumentModal } from "@/components/document-form-modal";
 
 interface TableRowData {
   id: number;
@@ -49,8 +53,8 @@ export const CompanyTableV2 = () => {
     document_id: null,
     file_name: null,
   });
-  console.log('openDocument', openDocument);
-  
+  console.log("openDocument", openDocument);
+
   const [rows, setRows] = React.useState<TableRowData[]>([
     {
       id: 1,
@@ -59,7 +63,7 @@ export const CompanyTableV2 = () => {
       frequency: "value1",
       description: "",
       document: "",
-      status: "value1",
+      status: "pending",
       filedDate: null,
       dueDate: null,
       reminder: "",
@@ -76,7 +80,7 @@ export const CompanyTableV2 = () => {
       frequency: "weekly",
       description: "Description",
       document: "",
-      status: "Pending",
+      status: "pending",
       filedDate: null,
       dueDate: null,
       reminder: "",
@@ -97,6 +101,14 @@ export const CompanyTableV2 = () => {
   ) => {
     setRows(
       rows.map((row) => (row.id === id ? { ...row, [field]: value } : row))
+    );
+  };
+
+  const handleEditRow = (id: number) => {
+    setRows(
+      rows.map((row) =>
+        row.id === id ? { ...row, isRowEditable: !row.isRowEditable } : row
+      )
     );
   };
 
@@ -143,34 +155,51 @@ export const CompanyTableV2 = () => {
               <React.Fragment key={row.id}>
                 <TableRow>
                   <TableCell>
-                    <CustomSelect
-                      value={row.docType}
-                      options={[
-                        { label: "Full accounts", value: "value1" },
-                        { label: "Dormant Company", value: "value2" },
-                      ]}
-                      onChange={(e: any) =>
-                        handleInputChange(row.id, "docType", e.target.value)
-                      }
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <FormControl fullWidth variant="standard">
+                    {row.isRowEditable ? (
                       <CustomSelect
-                        value={row.frequency}
+                        value={row.docType}
                         options={[
-                          { label: "annual", value: "value1" },
-                          { label: "monthly", value: "value2" },
-                          { label: "weekly", value: "value3" },
+                          { label: "Full accounts", value: "value1" },
+                          { label: "Dormant Company", value: "value2" },
                         ]}
                         onChange={(e: any) =>
-                          handleInputChange(row.id, "frequency", e.target.value)
+                          handleInputChange(row.id, "docType", e.target.value)
                         }
                       />
-                    </FormControl>
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.docType}
+                      </Typography>
+                    )}
                   </TableCell>
                   <TableCell>
-                    <TableCell>
+                    {row.isRowEditable ? (
+                      <FormControl fullWidth variant="standard">
+                        <CustomSelect
+                          value={row.frequency}
+                          options={[
+                            { label: "annual", value: "value1" },
+                            { label: "monthly", value: "value2" },
+                            { label: "weekly", value: "value3" },
+                          ]}
+                          onChange={(e: any) =>
+                            handleInputChange(
+                              row.id,
+                              "frequency",
+                              e.target.value
+                            )
+                          }
+                        />
+                      </FormControl>
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.frequency}
+                      </Typography>
+                    )}
+                  </TableCell>
+
+                  <TableCell>
+                    {row?.isRowEditable ? (
                       <CustomInputField
                         value={row.description || ""}
                         placeholder="Description"
@@ -182,106 +211,234 @@ export const CompanyTableV2 = () => {
                           )
                         }
                       />
-                    </TableCell>
-                  </TableCell>
-                  <TableCell>
-                    <Button
-                      onClick={() =>
-                        setOpenDocument({
-                          open: true,
-                          document_id: row.id,
-                          file_name: null,
-                        })
-                      }
-                      variant="outlined"
-                      component="label"
-                      sx={{
-                        border: "1px solid",
-                        borderColor: "text.primary",
-                        color: "text.secondary",
-                        textTransform: "none",
-                        borderRadius: 2,
-                        py: 0,
-                        px: 1,
-                      }}
-                    >
-                      <AttachFileIcon
-                        sx={{ transform: "rotate(45deg)", fontSize: 20 }}
-                      />
-                      {openDocument.file_name && openDocument.document_id === row.id
-                        ? openDocument.file_name
-                        : "View"}
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <CustomSelect
-                      value={row.status}
-                      options={[
-                        { label: "Draft", value: "value1" },
-                        { label: "Publish", value: "value2" },
-                        { label: "Pending", value: "value3" },
-                      ]}
-                      onChange={(e: any) =>
-                        handleInputChange(row.id, "status", e.target.value)
-                      }
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <DesktopDatePicker
-                      defaultValue={dayjs("2022-04-17")}
-                      value={row.filedDate}
-                      onChange={(date: any) =>
-                        handleInputChange(row.id, "filedDate", date)
-                      }
-                    />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.description}
+                      </Typography>
+                    )}
                   </TableCell>
 
                   <TableCell>
-                    <DesktopDatePicker
-                      defaultValue={dayjs("2022-04-17")}
-                      value={row.dueDate}
-                      onChange={(date: any) =>
-                        handleInputChange(row.id, "dueDate", date)
-                      }
-                    />
+                    {row?.isRowEditable ? (
+                      <Button
+                        onClick={() =>
+                          setOpenDocument({
+                            open: true,
+                            document_id: row.id,
+                            file_name: null,
+                          })
+                        }
+                        variant="outlined"
+                        component="label"
+                        sx={{
+                          border: "1px solid",
+                          borderColor: "text.primary",
+                          color: "text.secondary",
+                          textTransform: "none",
+                          borderRadius: 2,
+                          py: 0,
+                          px: 1,
+                        }}
+                      >
+                        <AttachFileIcon
+                          sx={{ transform: "rotate(45deg)", fontSize: 20 }}
+                        />
+                        {openDocument.file_name &&
+                        openDocument.document_id === row.id
+                          ? openDocument.file_name
+                          : "View"}
+                      </Button>
+                    ) : (
+                      <Button
+                        disabled
+                        onClick={() =>
+                          setOpenDocument({
+                            open: true,
+                            document_id: row.id,
+                            file_name: null,
+                          })
+                        }
+                        variant="outlined"
+                        component="label"
+                        sx={{
+                          border: "1px solid",
+                          borderColor: "text.primary",
+                          color: "text.secondary",
+                          textTransform: "none",
+                          borderRadius: 2,
+                          py: 0,
+                          px: 1,
+                        }}
+                      >
+                        <AttachFileIcon
+                          sx={{ transform: "rotate(45deg)", fontSize: 20 }}
+                        />
+                        {openDocument.file_name &&
+                        openDocument.document_id === row.id
+                          ? openDocument.file_name
+                          : "View"}
+                      </Button>
+                    )}
                   </TableCell>
                   <TableCell>
-                    <CustomInputField
-                      value={row.reminder || ""}
-                      placeholder="45"
-                      onChange={(e) =>
-                        handleInputChange(row.id, "reminder", e.target.value)
-                      }
-                    />
+                    {row.isRowEditable ? (
+                      <CustomSelect
+                        value={row.status}
+                        options={[
+                          { label: "Draft", value: "draft" },
+                          { label: "Publish", value: "publish" },
+                          { label: "Pending", value: "pending" },
+                        ]}
+                        onChange={(e: any) =>
+                          handleInputChange(row.id, "status", e.target.value)
+                        }
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        <Chip
+                          label={row.status}
+                          size="small"
+                          sx={{
+                            bgcolor: `${alpha("#367BF5", 0.3)}`,
+                            color: "#367BF5",
+                            fontWeight: 500,
+                            borderRadius: 2,
+                          }}
+                        />
+                      </Typography>
+                    )}
                   </TableCell>
                   <TableCell>
-                    <CustomSelect
-                      value={row.repeat}
-                      options={[
-                        { label: "Daily", value: "value1" },
-                        { label: "Weekly", value: "value2" },
-                      ]}
-                      onChange={(e: any) =>
-                        handleInputChange(row.id, "repeat", e.target.value)
-                      }
-                    />
+                    {row.isRowEditable ? (
+                      <DesktopDatePicker
+                        defaultValue={dayjs("2022-04-17")}
+                        value={row.filedDate}
+                        onChange={(date: any) =>
+                          handleInputChange(row.id, "filedDate", date)
+                        }
+                        slotProps={{
+                          textField: {
+                            variant: "standard",
+                            sx: {
+                              backgroundColor: "white",
+                              px: 1,
+                              borderRadius: 2,
+                            },
+                            InputProps: {
+                              disableUnderline: true,
+                            },
+                          },
+                        }}
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.filedDate
+                          ? dayjs(row.filedDate).format("DD/MM/YYYY")
+                          : "-"}
+                      </Typography>
+                    )}
+                  </TableCell>
+
+                  <TableCell>
+                    {row.isRowEditable ? (
+                      <DesktopDatePicker
+                        defaultValue={dayjs("2022-04-17")}
+                        value={row.dueDate}
+                        onChange={(date: any) =>
+                          handleInputChange(row.id, "dueDate", date)
+                        }
+                        slotProps={{
+                          textField: {
+                            variant: "standard",
+                            sx: {
+                              backgroundColor: "white",
+                              px: 1,
+                              borderRadius: 2,
+                            },
+                            InputProps: {
+                              disableUnderline: true,
+                            },
+                          },
+                        }}
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.filedDate
+                          ? dayjs(row.filedDate).format("DD/MM/YYYY")
+                          : "-"}
+                      </Typography>
+                    )}
                   </TableCell>
                   <TableCell>
-                    <DesktopDatePicker
-                      defaultValue={dayjs("2022-04-17")}
-                      value={row.lastUpdated}
-                      onChange={(date: any) =>
-                        handleInputChange(row.id, "lastUpdated", date)
-                      }
-                    />
+                    {row.isRowEditable ? (
+                      <CustomInputField
+                        value={row.reminder || ""}
+                        placeholder="45"
+                        onChange={(e) =>
+                          handleInputChange(row.id, "reminder", e.target.value)
+                        }
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.reminder}
+                      </Typography>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {row?.isRowEditable ? (
+                      <CustomSelect
+                        value={row.repeat}
+                        options={[
+                          { label: "Daily", value: "value1" },
+                          { label: "Weekly", value: "value2" },
+                        ]}
+                        onChange={(e: any) =>
+                          handleInputChange(row.id, "repeat", e.target.value)
+                        }
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.repeat}
+                      </Typography>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {row.isRowEditable ? (
+                      <DesktopDatePicker
+                        defaultValue={dayjs("2022-04-17")}
+                        value={row.lastUpdated}
+                        onChange={(date: any) =>
+                          handleInputChange(row.id, "lastUpdated", date)
+                        }
+                        slotProps={{
+                          textField: {
+                            variant: "standard",
+                            sx: {
+                              backgroundColor: "white",
+                              px: 1,
+                              borderRadius: 2,
+                            },
+                            InputProps: {
+                              disableUnderline: true,
+                            },
+                          },
+                        }}
+                      />
+                    ) : (
+                      <Typography color="text.secondary">
+                        {row.filedDate
+                          ? dayjs(row.filedDate).format("DD/MM/YYYY")
+                          : "-"}
+                      </Typography>
+                    )}
                   </TableCell>
                   <TableCell>
                     <Box sx={{ display: "flex", alignItems: "center" }}>
                       <IconButton onClick={() => handleDeleteRow(row.id)}>
                         <DeleteOutlineIcon />
                       </IconButton>
-                      <IconButton onClick={() => handleDeleteRow(row.id)}>
-                        <EditIcon />
+                      <IconButton onClick={() => handleEditRow(row.id)}>
+                        {row.isRowEditable ? <DoneIcon /> : <EditIcon />}
                       </IconButton>
                     </Box>
                   </TableCell>
